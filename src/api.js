60772f081e11bb05cd9cf01fd3963371288be1b4
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2022-11-11 01:12:49 +0000

    start

--- src/api.js
+++ src/api.js
@@ -1,30 +1,30 @@
-import axios from 'axios';
-// import PropTypes from 'prop-types';
+// import axios from 'axios';
+// // import PropTypes from 'prop-types';
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// example url = 'https://api.themoviedb.org/3/movie/550?api_key=857288c3c5f42347171bc7541b9a4b57'
-// https://api.themoviedb.org/3/search/movie?api_key=857288c3c5f42347171bc7541b9a4b57&language=en-US&page=1&include_adult=false&query=
-const BASE_URL = 'https://api.themoviedb.org/';
-const API_KEY = '857288c3c5f42347171bc7541b9a4b57';
+// // const BASE_URL = 'https://pixabay.com/api/';
+// // example url = 'https://api.themoviedb.org/3/movie/550?api_key=857288c3c5f42347171bc7541b9a4b57'
+// // https://api.themoviedb.org/3/search/movie?api_key=857288c3c5f42347171bc7541b9a4b57&language=en-US&page=1&include_adult=false&query=
+// const BASE_URL = 'https://api.themoviedb.org/';
+// const API_KEY = '857288c3c5f42347171bc7541b9a4b57';
 
-export const fetchMovies = async ({ query }) => {
-  // const per_page = 12;
+// export const fetchMovies = async ({ query }) => {
+//   // const per_page = 12;
 
-  const url = `${BASE_URL}3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`;
-  //https://api.themoviedb.org/cat?api_key=857288c3c5f42347171bc7541b9a4b57
-  try {
-    const response = await axios.get(url);
+//   const url = `${BASE_URL}3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`;
+//   //https://api.themoviedb.org/cat?api_key=857288c3c5f42347171bc7541b9a4b57
+//   try {
+//     const response = await axios.get(url);
 
-    // const movies = getNormalizedMovies(response.data.hits);
-    // const pages = Math.ceil(response.data.totalHits / per_page);
+//     // const movies = getNormalizedMovies(response.data.hits);
+//     // const pages = Math.ceil(response.data.totalHits / per_page);
 
-    // return { movies };
-    return response;
-  } catch (error) {
-    throw new Error(error);
-    console.error(error);
-  }
-};
+//     // return { movies };
+//     return response;
+//   } catch (error) {
+//     throw new Error(error);
+//     console.error(error);
+//   }
+// };
 
 // const getNormalizedMovies = movies => {
 //   const normalizedData = movies.map(
